bb7a0c49f49e6607dce012981500a71f24e6060b
Felipe <felipe@athenasagricola.com.br>
2021-11-01 00:30:17 -0300

    add prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main () {
+  const user = await prisma.user.create({
+    data: {
+      name: 'felipe@athenasagricola.com.br',
+      email: 'felipe@athenasagricola.com.br',
+      idQlik: 'x3UR2mhwP3s6rRvjEsh55P5IFQg18Pvy',
+      isAdmin: true,
+      licenseQlik: 'professional'
+    }
+  })
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
