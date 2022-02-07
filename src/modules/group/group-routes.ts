d16946bda9d6e95a4d0348ddd643d3ddd3aa9d0d
Felipe <felipe@athenasagricola.com.br>
2022-02-07 15:30:22 -0300

    creating put end-point

--- src/modules/group/group-routes.ts
+++ src/modules/group/group-routes.ts
@@ -5,9 +5,11 @@ import { adaptRoute } from '@shared/adapters/http/express-route-adapter'
 import { Router } from 'express'
 
 import { makeCreateGroupController } from './factories/make-create-group-controller'
+import { makeEditGroupController } from './factories/make-edit-group-controller'
 import { makeGetGroupsBySpaceIdController } from './factories/make-get-groups-by-user-id-controller'
 
 export default (router: Router): void => {
   router.post('/group', [auth, isAdmin, space], adaptRoute(makeCreateGroupController()))
   router.get('/group', [auth, space], adaptRoute(makeGetGroupsBySpaceIdController()))
+  router.put('/group/:id', [auth, isAdmin, space], adaptRoute(makeEditGroupController()))
 }
