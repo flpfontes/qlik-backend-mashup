17e65f5097d46e6a4d10bad15debdaf523725629
Felipe <felipe@athenasagricola.com.br>
2022-02-07 15:29:39 -0300

    creating factory edit group controller

---
+++ src/modules/group/factories/make-edit-group-controller.ts
@@ -0,0 +1,16 @@
+import { makeSlugifyAdapter } from '@shared/factories/make-slugify-adapter'
+import { Controller } from '@shared/presentation/protocols/controller'
+
+import { EditGroupController } from '../presentation/controllers/edit-group'
+import { GroupRepository } from '../repository/prisma/space'
+import { EditGroupUseCase } from '../use-cases/edit-group'
+import { makeCreateGroupValidation } from './make-create-group-validation'
+
+export const makeEditGroupController = (): Controller => {
+  const groupRepository = new GroupRepository()
+  const slugAdapter = makeSlugifyAdapter()
+  const createSpaceUseCase = new EditGroupUseCase(groupRepository, slugAdapter)
+  const validation = makeCreateGroupValidation()
+  const controller = new EditGroupController(validation, createSpaceUseCase)
+  return controller
+}
