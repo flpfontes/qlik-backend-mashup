6e4ff661416e4c0dc2b953bb2865b5086e86dec6
Felipe <felipe@athenasagricola.com.br>
2022-02-07 13:54:21 -0300

    creating controller to delete space by id

---
+++ src/modules/space/presentation/controllers/delete-space.ts
@@ -0,0 +1,29 @@
+import { SpaceRepository } from '@modules/space/repository/prisma/space'
+import {
+  noContent,
+  serverError
+} from '@shared/presentation/helpers/http'
+import { Controller } from '@shared/presentation/protocols/controller'
+import { HttpResponse } from '@shared/presentation/protocols/http'
+
+type Request = {
+  id:string
+}
+
+export class DeletepaceController implements Controller {
+  constructor (
+    private readonly spaceRepository: SpaceRepository
+  ) {}
+
+  async handle (request: Request): Promise<HttpResponse> {
+    try {
+      const { id } = request
+
+      await this.spaceRepository.delete({ id })
+
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
