b16d6865d6cc2211e2dae53f7ecfc46a688b67be
Felipe <felipe@athenasagricola.com.br>
2022-02-07 16:04:27 -0300

    fix import group repository

--- src/modules/group/factories/make-get-groups-by-user-id-controller.ts
+++ src/modules/group/factories/make-get-groups-by-user-id-controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@shared/presentation/protocols/controller'
 
 import { GetGroupsBySpaceIdController } from '../presentation/controllers/get-groups-by-space-id'
-import { GroupRepository } from '../repository/prisma/space'
+import { GroupRepository } from '../repository/prisma/group'
 import { GetGroupsBySpaceIdUseCase } from '../use-cases/get-groups-by-space-id'
 
 export const makeGetGroupsBySpaceIdController = (): Controller => {
