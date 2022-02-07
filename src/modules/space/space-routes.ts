407799dda2a98b836baf502815e0486308c17829
Felipe <felipe@athenasagricola.com.br>
2022-02-07 13:49:46 -0300

    creating put end-point

--- src/modules/space/space-routes.ts
+++ src/modules/space/space-routes.ts
@@ -4,9 +4,11 @@ import { adaptRoute } from '@shared/adapters/http/express-route-adapter'
 import { Router } from 'express'
 
 import { makeCreateSpaceController } from './factories/make-create-space-controller'
+import { makeEditSpaceController } from './factories/make-edit-space-controller'
 import { makeGetSpacesByUserIdController } from './factories/make-get-spaces-by-user-id-controller'
 
 export default (router: Router): void => {
   router.post('/space', [auth, isAdmin], adaptRoute(makeCreateSpaceController()))
   router.get('/space', auth, adaptRoute(makeGetSpacesByUserIdController()))
+  router.put('/space/:id', [auth, isAdmin], adaptRoute(makeEditSpaceController()))
 }
